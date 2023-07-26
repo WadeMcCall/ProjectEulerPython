d49613407cd4fb6730a7cc6f36cdb0ecbb35f2a2
WadeMcCall <danceifuwantto@gmail.com>
2023-07-26 10:46:47 -0700

    solution #99

---
+++ LargestExponential.py
@@ -0,0 +1,25 @@
+import math
+
+# return the number of digits in the evaluated expression
+def findMagnitude(base, exp):
+    return (exp * math.log10(base)) + 1
+
+def main():
+    file = open("data/base_exp.txt", "r")
+    max = 0
+    maxline = 0
+    i = 1
+    for line in file:
+        words = line.split(",")
+        base = int(words[0])
+        exp = int(words[1])
+        magnitude = (findMagnitude(base, exp))
+        if magnitude >= max:
+            max = magnitude
+            maxline = i
+        i += 1
+    print(maxline)
+    return
+
+if __name__ == '__main__':
+    main()
